a2d2ec2ab55e7779f535e02753bf0a0266d13509
AlexNuclear <Peaceatom@bk.ru>
2016-09-08 19:01:06 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+<script>
+
+var link = document.querySelector(".search-button");
+var popup = document.querySelector(".searcher-button");
+
+link.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.toggle("searcher-button-show");
+});
+
+</script>
